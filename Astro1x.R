6148b96ffbb82b958036797ab12b53d618248784
maurice60 <maurice.williams@me.com>
2015-02-08 15:17:01 +0000

    Start Astro2x

--- Astro1x.R
+++ Astro1x.R
@@ -1,20 +1,5 @@
-pc <- 3.09e16 # Parsec to m
-Mpc <- 3.09e22 # Megaparsec in m
-ms <- 2e30 # Mass of the sun
-Ls <- 3.839e26 # Luminosity of the Sun
-AU <- 1.496e11 # Astronomical unit in m
-c <- 3e8 # Speed of light in a vacuum
-eV <- 1.6e-19 # electron volt J
-ep <- 1e-3 # present day mean energy of photons eV
-rho0 <- 5e-28 # present day density of universe
-Ep <- 1.2e28*eV #Planck energy
-kWh <- 3.6e6 # J in a kW/h
-G <- 6.67384e-11 # Gravitational constant
-sb <- 5.67e-8 #Stefan-Boltzmann constant
+source('constant.R')
 
-# Degreees to radians / radians to degrees
-rad <- function(d) d*pi/180
-deg <- function(r) r*180/pi
 # Luminosity given flux and distance
 L <- function(f, D) f * 4 * pi * D^2
 # Flux given luminosity and distance
